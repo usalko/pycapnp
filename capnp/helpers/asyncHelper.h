9285ec873ea53677ceb10d0d53911ab0e1be2db4
Vanya U <ivict@rambler.ru>
2023-07-31 23:16:22 +0300

    # I've tried another approach.

--- capnp/helpers/asyncHelper.h
+++ capnp/helpers/asyncHelper.h
@@ -59,14 +59,17 @@ void waitVoidPromise(kj::Promise<void> *promise, kj::WaitScope &scope)
     // auto obj = wrap_kj_exception_for_reraise(exn);
     // PyErr_SetObject((PyObject*)obj->ob_type, obj);
     // Py_DECREF(obj);
-    PyErr_SetString(PyExc_RuntimeError, exn.getDescription().cStr());
+    // PyErr_SetString(PyExc_RuntimeError, exn.getDescription().cStr());
+    PyErr_SetObject(PyExc_RuntimeError, exn.getDescription().cStr());
   }
   catch (const std::exception& exn) {
-    PyErr_SetString(PyExc_RuntimeError, exn.what());
+    // PyErr_SetString(PyExc_RuntimeError, exn.what());
+    PyErr_SetObject(PyExc_RuntimeError, exn.what());
   }
   catch (...)
   {
-    PyErr_SetString(PyExc_RuntimeError, "Unknown exception");
+    // PyErr_SetString(PyExc_RuntimeError, "Unknown exception");
+    PyErr_SetObject(PyExc_RuntimeError, "Unknown exception");
   }
 }
 
@@ -81,17 +84,20 @@ PyObject *waitPyPromise(kj::Promise<PyObject *> *promise, kj::WaitScope &scope)
     // auto obj = wrap_kj_exception_for_reraise(exn);
     // PyErr_SetObject((PyObject*)obj->ob_type, obj);
     // Py_DECREF(obj);
-    PyErr_SetString(PyExc_RuntimeError, exn.getDescription().cStr());
-    return NULL;
+    // PyErr_SetString(PyExc_RuntimeError, exn.getDescription().cStr());
+    PyErr_SetObject(PyExc_RuntimeError, exn.getDescription().cStr());
+    return NULL:
   }
   catch (const std::exception& exn) {
-    PyErr_SetString(PyExc_RuntimeError, exn.what());
-    return NULL;
+    // PyErr_SetString(PyExc_RuntimeError, exn.what());
+    PyErr_SetObject(PyExc_RuntimeError, exn.what());
+    return NULL:
   }
   catch (...)
   {
-    PyErr_SetString(PyExc_RuntimeError, "Unknown exception");
-    return NULL;
+    // PyErr_SetString(PyExc_RuntimeError, "Unknown exception");
+    PyErr_SetObject(PyExc_RuntimeError, "Unknown exception");
+    return NULL:
   }
 }
 
